657dd30e9a19b14a03a6e41e02e78ec85838126d
Mark Sturdevant <mark.sturdevant@ibm.com>
2018-07-19 20:21:00 -0700

    initial code pattern rework (#4)
    * Readme w/ deploy button w-i-p
    
    * manifest typo
    
    * manifesting
    
    * action template
    
    * Updates
    
    * README work, rent-a-car agent deploy, no redis
    
    * Lots of dev work
    * README getting filled out
    * Code to upload conversation (rent-a-car)
    * No need for redis, use conversationToken
    * Fix cancel handling
    * Concersation -> Assistant
    
    * Conversation -> Assistant
    
    * Use renamed lib
    * Rename vars...
    
    * Add Travis and eslint.
    
    Setup eslint and travis.
    Travis should fail on lint.
    Update some packages.
    
    * Bump node versions to current. Fix lint.
    
    * prettier code
    * require node.js 6 or later
    * test on current versions of node.js
    
    * More README. Add invocation name.
    
    * Add a step to set the invocation name
    * Other small tweaks

--- README.md
+++ README.md
@@ -1,62 +1,141 @@
-# Watson Conversation & Google Home
-A code allowing you to demo Watson Conversation with Google Home.
+<!--
+[![Build Status](https://travis-ci.org/IBM/watson-google-assistant.svg?branch=master)](https://travis-ci.org/IBM/watson-google-assistant)
+-->
 
-## Components of the solution
+# Create an Action for Google Assistant using Watson Assistant
 
-### Watson Conversation
-Watson Conversation service combines machine learning, natural language understanding, and integrated dialog tools to create conversation flows between your apps and your users.
+This Code Pattern includes a Watson Assistant workspace to demonstrate an implementation of a retail agent that can ask for reservation schedules and specifics. To demonstrate how to test it with Google Assistant devices, we will setup a Google Action that calls out to our Node.js server which interacts with Watson Assistant.
 
-### Google Home
-Powered by the Google Assistant, with [Google Home](https://support.google.com/googlehome/answer/7029281?hl=en) you have command of your music, your home, and your curiosity. Start by saying, for example “Ok Google" or "Hey Google.” in English.
+When the reader has completed this Code Pattern, they will understand how to:
 
-## Before you begin
+* Create a Google Assistant Action
+* Create a Node.js Express web application
+* Interact with Watson Assistant
+* Tie it all together
 
-* Create a Bluemix account
-    * [Sign up](https://console.ng.bluemix.net/registration/?target=/catalog/%3fcategory=watson) in Bluemix, or use an existing account. 
-* A Watson Conversation Instance with one workspace
-* A Redis account. Redis is an open-source, blazingly fast, key/value low maintenance store. I use it to store conversation context. 
-    * You can instantiate one instance as an IBM Cloud service [here](https://console.bluemix.net/catalog/services/compose-for-redis?taxonomyNavigation=apps)
-* A Google Actions project (https://developers.google.com/actions/)
+![](doc/source/images/architecture.png)
 
-## Deploy Node.js app on Bluemix
+## Flow
+1. User talks or types to Google Assistant.
+2. Google Assistant posts text to an HTTPS endpoint.
+3. Node.js server calls Watson Assistant to get the response.
+4. The response is returned to Google Assistant.
+5. Google Assistant replies to the user.
 
-Follow documentation in that [Github project](https://github.com/watson-developer-cloud/conversation-simple).
+## Included components
+* [Google Home](https://support.google.com/googlehome/answer/7029281?hl=en): Powered by the Google Assistant, with [Google Home](https://support.google.com/googlehome/answer/7029281?hl=en) you have command of your music, your home, and your curiosity. Start by saying, for example “Ok Google" or "Hey Google” in English.
+* [Watson Assistant](https://www.ibm.com/watson/developercloud/conversation.html): Create a chatbot with a program that conducts a conversation via auditory or textual methods.
 
-* Create an .env file for credentials and connections details
-```
-WCS_Username=
-WCS_Password=
-workspace_id=
-redis_port=
-redis_url=
-redis_auth=  
-```
+## Featured technologies
+* [Node.js](https://nodejs.org/): An open-source JavaScript run-time environment for executing server-side JavaScript code.
 
-## Setup Google Actions Project
 
-1. Go to [Google Actions Console](https://console.actions.google.com)
-2. Add/Import a project
-3. Build your app using Actions SDK. Click **BUILD**
+# Watch the Video
+[![Watson and Google together](https://user-images.githubusercontent.com/9534938/33854699-e2628078-dec2-11e7-9366-e9e698160728.png)](https://www.youtube.com/watch?v=-lIxmYxLzbA "Watson & Google together")
 
-![options](https://developers.google.com/actions/images/aog-project-apiai-actions.png)
+# Steps
 
-4. Download [gactions](https://developers.google.com/actions/tools/gactions-cli) command line tool
-I have already create 3 actions for English, French (France) & French (Canada).
-5. Modify these files :
-  * description
-  * queryPatterns
-  * name & url - depends on your Bluemix deployment 
-6. Launch these commands
-  * gactions update --action_package action_fr3.json --action_package action_fr.json --action_package action.json --action_package action_fr2.json --project **[YOUR_PROJECT_ID]**
-  * gactions test --action_package action_fr3.json --action_package action_fr.json --action_package action.json --action_package action_fr2.json --project **[YOUR_PROJECT_ID]**
-7. You have to finalize the app configuration for production, click [here](https://developers.google.com/actions/sdk/submit-app)
+1. [Deploy to IBM Cloud](#deploy-to-ibm-cloud)
 
-In order to test it you have three options:
-- Web Simulator
-- Google Assistant mobile app 
-- a Google Home
+1. [Setup Google Actions](#setup-google-actions)
 
-If you are in Testing Home, you have to invocate the Google Actions by using "Talk to **APP_NAME**" or "Parler avec **APP_NAME**.
+1. [Talk to it!](#talk-to-it)
+
+## Deploy to IBM Cloud
+[![Deploy to IBM Cloud](https://bluemix.net/deploy/button.png)](https://bluemix.net/deploy?repository=https://github.com/IBM/watson-google-assistant.git)
+
+1. Press the above `Deploy to IBM Cloud` button and then click on `Deploy`.
+
+2. In Toolchains, click on Delivery Pipeline to watch while the app is deployed.
+
+3. To see the app and service created and configured for this Code Pattern, use the IBM Cloud dashboard. The app is named `watson-google-assistant` with a unique suffix. The following service is created and easily identified by the `wga-` prefix:
+    * wga-assistant
+
+## Setup Google Actions
+
+1. Clone the repo
+
+   Clone the `watson-google-assistant` repo locally. In a terminal, run:
+
+   ```
+   git clone https://github.com/IBM/watson-google-assistant
+   ```
+
+1. Go to [Actions on Google Developer Console](https://console.actions.google.com)
+
+1. Create your project
+   * Click on `+ Add/import project`
+   * Enter a project name
+   * Choose the default language for your Actions
+   * Select your country or region
+   * Click on `CREATE PROJECT`
+   * Click on `SKIP` to choose a category later
+
+1. Obtain your project ID
+   * Next to the `Overview` menu item, click on the gear icon and then `Project settings`.
+   * Save the `Project ID` to use later.
+
+1. Set the invocation name
+
+   * Use the left sidebar menu to select `SETUP` > `Invocation`.
+   * Enter a display name. Users will say or type this name to explicitly invoke your action.
+   * Hit `SAVE`.
+
+1. Install the `gactions` CLI
+   * Download the `gactions` CLI from [here](https://developers.google.com/actions/tools/gactions-cli).
+   * `chmod` the `gactions` file to make it executable.
+   * Copy the `gactions` file into your local repo's `actions` directory.
+     ```
+     # For example, depending on your download and repo directories...
+
+     chmod +x ~/Downloads/gactions
+     cp ~/Downloads/gactions ~/watson-google-assistant/actions/
+     ```
+
+1. Edit the `actions/actions.json` file in your local repo.
+   * Edit the `url` using your deployed IBM Cloud app URL. Typically, you would just modify the timestamp digits.
+     > Note: URL needs `https://` prefix e.g. https://watson-google-assistant-20180707012345678.mybluemix.net/ 
+
+   <!-- TODO: Not needed(?), but should add implicit queryPatterns
+   * queryPatterns
+   * name
+   * description
+   -->
+
+1. Create the action using the CLI
+   > Note: If/when it prompts you to enter an authorization code, browse to the provided URL to login and authorize the CLI to use your account and copy/paste the auth code at the prompt.
+
+   * Run the `gactions` command to update your action and prepare it for testing. Use the project ID you saved earlier.
+
+     ```
+     cd ~/watson-google-assistant/actions/
+     ./gactions update --action_package action.json --project <YOUR_PROJECT_ID>
+     ./gactions test --action_package action.json --project <YOUR_PROJECT_ID>
+     ```
+
+## Talk to it!
+
+1. Test it in the simulator
+
+   * Go back to your Actions on Google Developer Console
+   * Use the left sidebar menu to select `TEST` > `Simulator` and start testing
+   * Type in the `Input` box or click on the microphone icon to use voice input
+   * Say "Talk to my test app" or "Talk to \<your app name\>" to initiate the converation
+
+1. Try it with your phone, your Google Home, or other device
+
+   * Log into the device with the same account you used to create your test app
+   * Say "Hey Google, talk to \<your app name\>"
+
+   <!--
+    TODO: 1. Test it on smart phone, etc...
+    TODO: 1. Using the Rental Car Assistant
+    TODO: 1. Using a different conversation
+    TODO: 1. Using another language
+    TODO: 1. You have to finalize the app configuration for production, click [here](https://developers.google.com/actions/sdk/submit)
+   -->
+
+# Sample output
 
 ### Illustration of the Web Simulator
 
@@ -70,11 +149,16 @@ If you are in Testing Home, you have to invocate the Google Actions by using "Ta
 
 ![img_d4d247ea81fc-1](https://user-images.githubusercontent.com/9534938/33787305-eea71f7c-dc6c-11e7-8710-1636e5a91b2f.jpeg)
 
-## License
+# Links
+* [Demo on Youtube](https://www.youtube.com/watch?v=TODO)
+* [Create an Alexa skill with Watson Assistant](https://developer.ibm.com/code/patterns/create-an-alexa-skill-with-serverless-and-a-conversation/)
+* [Watson Node.js SDK](https://github.com/watson-developer-cloud/node-sdk)
 
-This sample code is licensed under Apache 2.0.
-Full license text is available in [LICENSE](LICENSE).
+# Learn more
 
-## Contributing
+* **Artificial Intelligence Code Patterns**: Enjoyed this Code Pattern? Check out our other [AI Code Patterns](https://developer.ibm.com/code/technologies/artificial-intelligence/).
+* **AI and Data Code Pattern Playlist**: Bookmark our [playlist](https://www.youtube.com/playlist?list=PLzUbsvIyrNfknNewObx5N7uGZ5FKH0Fde) with all of our Code Pattern videos
+* **With Watson**: Want to take your Watson app to the next level? Looking to utilize Watson Brand assets? [Join the With Watson program](https://www.ibm.com/watson/with-watson/) to leverage exclusive brand, marketing, and tech resources to amplify and accelerate your Watson embedded commercial solution.
 
-See [CONTRIBUTING](CONTRIBUTING.md).
+# License
+[Apache 2.0](LICENSE)
--- index.js
+++ index.js
@@ -1,189 +1,193 @@
+/**
+ * Copyright 2017 IBM Corp. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the 'License'); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 'use strict';
 
-const ConversationV1 = require('watson-developer-cloud/conversation/v1');
-const redis = require('redis');
+const AssistantV1 = require('watson-developer-cloud/assistant/v1');
+const bodyParser = require('body-parser');
+const express = require('express');
+const fs = require('fs');
+const jwt = require('jsonwebtoken');
+const secret = 'notsecret';
 
-var express = require('express');
-var app = express();
-var port = process.env.PORT || 8080;
-var bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT || 8080;
 
 require('dotenv').config();
+
+const DEFAULT_NAME = 'rent-a-car';
+const WatsonAssistantSetup = require('./lib/watson-assistant-setup');
+let setupError = '';
+
+/**
+ * Handle setup errors by logging and appending to the global error text.
+ * @param {String} reason - The error message for the setup error.
+ */
+function handleSetupError(reason) {
+  setupError += ' ' + reason;
+  console.error('The app failed to initialize properly. Setup and restart needed.' + setupError);
+  // We could allow our chatbot to run. It would just report the above error.
+  // Or we can add the following 2 lines to abort on a setup error allowing Bluemix to restart it.
+  console.error('\nAborting due to setup error!');
+  process.exit(1);
+}
+
+// Connect a client to Watson Assistant
+// The SDK gets credentials from the environment.
+const assistant = new AssistantV1({ version: '2018-02-16' });
+console.log('Connected to Watson Assistant');
+let workspaceID; // workspaceID will be set when the workspace is created or validated.
+const assistantSetup = new WatsonAssistantSetup(assistant);
+const workspaceJson = JSON.parse(fs.readFileSync('data/assistant/workspaces/rent_a_car.json'));
+const assistantSetupParams = { default_name: DEFAULT_NAME, workspace_json: workspaceJson };
+assistantSetup.setupAssistantWorkspace(assistantSetupParams, (err, data) => {
+  if (err) {
+    handleSetupError(err);
+  } else {
+    console.log('Watson Assistant is ready!');
+    workspaceID = data;
+  }
+});
 
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-// Using some globals for now
-let conversation;
-let redisClient;
 let context;
 let Wresponse;
+let expectUserResponse;
 
-function errorResponse(reason) {
-	return {
-	  version: '1.0',
-	  response: {
-		shouldEndSession: true,
-		outputSpeech: {
-		  type: 'PlainText',
-		  text: reason || 'An unexpected error occurred. Please try again later.'
-		}
-	  }
-	};
+/**
+ * Forword input text and stored context to Watson Assistant and
+ * return the response.
+ *
+ * @param {*} request - incoming request
+ * @param {*} workspaceId - Watson Assistant workspace ID
+ */
+function assistantMessage(request, workspaceId) {
+  if (!workspaceId) {
+    const msg = 'Error talking to Watson Assistant. Workspace ID is not set.';
+    console.error(msg);
+    return Promise.reject(msg);
   }
+  return new Promise(function(resolve, reject) {
+    console.log('REQUEST:');
+    console.log(request);
+    const input = request.inputs[0] ? request.inputs[0].rawInputs[0].query : 'hello';
 
-function initClients() {
-	return new Promise(function(resolve, reject) {
-	// Connect a client to Watson Conversation
-	conversation = new ConversationV1({
-		password: process.env.WCS_Password,
-        username: process.env.WCS_Username,
-		version_date: '2016-09-20'
-	});
-	console.log('Connected to Watson Conversation');
-  
-	  // Connect a client to Redis 
-	  redisClient = redis.createClient(process.env.redis_port, process.env.redis_url);
-	  redisClient.auth(process.env.redis_auth, function (err) {
-		if (err) throw err;
-	});
-	redisClient.on('connect', function() {
-		console.log('Connected to Redis');
-	});
-	resolve("Done");
+    // Use conversationToken to track Watson Assistant context
+    if (request.conversation && request.conversation.conversationToken) {
+      context = jwt.verify(request.conversation.conversationToken, secret);
+      console.log(context);
+    } else {
+      context = {};
+    }
+
+    // Forward input text to Watson Assistant
+    assistant.message(
+      {
+        input: { text: input },
+        workspace_id: workspaceId,
+        context: context
+      },
+      function(err, watsonResponse) {
+        if (err) {
+          console.error(err);
+          reject('Error talking to Watson Assistant.');
+        } else {
+          console.log(watsonResponse);
+          context = watsonResponse.context; // Update global context
+          resolve(watsonResponse);
+        }
+      }
+    );
   });
-  }
+}
 
-function conversationMessage(request, workspaceId) {
-	return new Promise(function(resolve, reject) {
-	  const input = request.inputs[0] ? request.inputs[0].rawInputs[0].query : 'start skill';
-		var test = {
-			input: { text: input },
-			workspace_id: workspaceId,
-			context: context
-			//context: {}
-		  };
-	  console.log("Input" + JSON.stringify(test,null,2));
-	  conversation.message(
-		{
-		  input: { text: input },
-		  workspace_id: workspaceId,
-		  context: context
-		},
-		function(err, watsonResponse) {
-		  if (err) {
-			console.error(err);
-			reject('Error talking to Watson.');
-		  } else {
-			console.log(watsonResponse);
-			context = watsonResponse.context; // Update global context			
-			resolve(watsonResponse);
-		  }
-		}
-	  );
-	});
-  }
-
-function getSessionContext(sessionId) {
-	console.log('sessionId: ' + sessionId); 
-	return new Promise(function(resolve, reject) {
-	  redisClient.get(sessionId, function(err, value) {
-		if (err) {
-		  console.error(err);
-		  reject('Error getting context from Redis.');
-		}
-		// set global context
-		context = value ? JSON.parse(value) : {};
-		console.log('---------');
-		console.log('Context Recupéré:');
-		console.log(context);
-		console.log('---------');
-		resolve();
-	  });
-	});
-  }
-  
-  function saveSessionContext(sessionId) {
-		console.log('---------');
-		console.log('Begin saveSessionContext ' + sessionId);
-  
-	// Save the context in Redis. Can do this after resolve(response).
-	if (context) {
-	  const newContextString = JSON.stringify(context);
-	  // Saved context will expire in 600 secs.
-	  redisClient.set(sessionId, newContextString, 'EX', 600);
-	  console.log('Saved context in Redis');
-	  console.log(sessionId);
-		console.log(newContextString);
-		console.log('---------');
-	}
-  }
-
+/**
+ * Prepare/resolve the response for Google Assistant.
+ *
+ * @param {*} response - Response from Watson Assistant
+ * @param {*} resolve - Promise resolve to complete
+ */
 function sendResponse(response, resolve) {
-	
-	  // Combine the output messages into one message.
-	  const output = response.output.text.join(' ');
-	  var resp = {
-		conversationToken: null,
-		expectUserResponse: true,
-		expectedInputs: [
-			{
-				inputPrompt: {
-					richInitialPrompt: {
-						items: [
-							{
-								simpleResponse: {
-									textToSpeech: output,
-									displayText: output
-								}
-							}
-						],
-						suggestions: []
-					}
-				},
-				possibleIntents: [
-					{
-						intent: 'actions.intent.TEXT'
-					}
-				]
-			}
-		]
-	};
-	
-	Wresponse =  resp;
-	// Resolve the main promise now that we have our response
-	resolve(resp);
-	}
+  // store context in conversationToken
+  const conversationToken = jwt.sign(context, secret);
+
+  // Combine the output messages into one message.
+  const output = response.output.text.join(' ');
+  const richResponse = {
+    items: [
+      {
+        simpleResponse: {
+          textToSpeech: output
+        }
+      }
+    ],
+    suggestions: []
+  };
+  const resp = {
+    conversationToken: conversationToken,
+    expectUserResponse: expectUserResponse
+  };
+
+  if (expectUserResponse) {
+    resp.expectedInputs = [
+      {
+        inputPrompt: {
+          richInitialPrompt: richResponse
+        },
+        possibleIntents: [
+          {
+            intent: 'actions.intent.TEXT'
+          }
+        ]
+      }
+    ];
+  } else {
+    const s = output.substring(0, 59); // Has to be < 60 chars.  :(
+    resp.finalResponse = { speechResponse: { textToSpeech: s } };
+  }
+
+  console.log(resp);
+  Wresponse = resp;
+  // Resolve the main promise now that we have our response
+  resolve(resp);
+}
 
 app.post('/api/google4IBM', function(args, res) {
-	return new Promise(function(resolve, reject) {
-	  const request = args.body;
-	  console.log("Google Home is calling");
-	  console.log(JSON.stringify(request,null,2));
-	  const sessionId = args.body.conversation.conversationId;
-	  initClients()
-	  .then(() => getSessionContext(sessionId))
-	  .then(() => conversationMessage(request, process.env.workspace_id))
-	  .then(actionResponse => sendResponse(actionResponse, resolve))
-	  .then(data => {
-		res.setHeader('Content-Type', 'application/json');
-		res.append("Google-Assistant-API-Version", "v2");
-		res.json(Wresponse);
-	})
-	.then(() => saveSessionContext(sessionId))    
-	.catch(function (err) {
-		console.error('Erreur !');
-		console.dir(err);
-	});
-	});
+  return new Promise(function(resolve, reject) {
+    const request = args.body;
+    console.log('Google Home is calling');
+    console.log(JSON.stringify(request, null, 2));
+    // Expect response must be false for action.intent.CANCEL
+    expectUserResponse = !(request.inputs[0] && request.inputs[0].intent === 'actions.intent.CANCEL');
+    assistantMessage(request, workspaceID)
+      .then(actionResponse => sendResponse(actionResponse, resolve))
+      .then(data => {
+        res.setHeader('Content-Type', 'application/json');
+        res.append('Google-Assistant-API-Version', 'v2');
+        res.json(Wresponse);
+      })
+      .catch(function(err) {
+        console.error('Erreur !');
+        console.dir(err);
+      });
   });
+});
 
-/*
-	res.setHeader('Content-Type', 'application/json')
-	res.append("Google-Assistant-API-Version", "v2")
-*/
-
-
-// start the server
+// start the http server
 app.listen(port);
 console.log('Server started! At http://localhost:' + port);
